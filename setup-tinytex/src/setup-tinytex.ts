3578ae6c25162783f65d8f9ffa18e113b7a85e82
Jonathan Keane <jkeane@gmail.com>
2019-12-01 19:24:31 -0600

    More powershely way

--- setup-tinytex/src/setup-tinytex.ts
+++ setup-tinytex/src/setup-tinytex.ts
@@ -95,7 +95,7 @@ async function installTinyTexWindows() {
   }
 
   // Cleanse install-windows.bat of the pause at the end to make it non-interactive
-  await exec.exec(`cat ${downloadPath} | % { $_ -replace "^pause$","" } > ${downloadPath}`)
+  await exec.exec(`((Get-Content -path ${downloadPath} -Raw) -replace '^pause$','') | Set-Content -Path ${downloadPath}`)
 
   await io.mv(downloadPath, path.join(tempDirectory, fileName));
 
